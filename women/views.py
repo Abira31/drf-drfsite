eb5e7e17e3375996a97f60d69e435c1824411821
artur <tyktybaev_ad@okb2-tmn.ru>
2023-02-21 15:59:31 +0500

    add pagination

--- women/views.py
+++ women/views.py
@@ -3,12 +3,14 @@ from rest_framework import permissions
 from .models import Women
 from .serializers import WomenSerializerList,WomenSerializer
 from .permissions import IsAdminOrReadOnly,IsWonerOrReadOnly
+from .pagination import WomenAPIListPagination
 
 
 class WomenAPIList(generics.ListAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializerList
     name = 'womens-list'
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPICreate(generics.CreateAPIView):
