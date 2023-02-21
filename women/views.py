463e0829c69d1b34c1b366f7116e158d7e5d2d77
artur <tyktybaev_ad@okb2-tmn.ru>
2023-02-21 10:51:23 +0500

    update file views

--- women/views.py
+++ women/views.py
@@ -1,18 +1,18 @@
-from rest_framework import viewsets,mixins,generics
+from rest_framework import generics
 
 from .models import Women
 from .serializers import WomenSerializerList,WomenSerializer
 
 
-class WomenListAPIView(mixins.ListModelMixin,viewsets.GenericViewSet):
+class WomenAPIList(generics.ListAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializerList
 
-class WomenAPIViewSet(mixins.CreateModelMixin,
-                      mixins.RetrieveModelMixin,
-                      mixins.UpdateModelMixin,
-                      viewsets.GenericViewSet):
+
+class WomenAPICreate(generics.CreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
-
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
